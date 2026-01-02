c84290d05b019722d5d95640b254b22df5651fab
sumanthreddykakarla-tech <sumanthreddykakarla@gmail.com>
2026-01-02 10:34:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    export default defineConfig({
+  plugins: [react()],
+  base: '/', // Add this line if it's missing!
+})
     return {
       server: {
         port: 3000,
